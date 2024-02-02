fff8e83cd25df1fa22b032e540eb42f76f14a93b
Gabriel <99262377+GabrielG-tech@users.noreply.github.com>
2024-02-02 09:29:31 -0300

    primeiro commit

---
+++ CRUD/CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CRUD.Models;
+
+namespace CRUD.Data {
+    public class ApplicationDbContext: IdentityDbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+        }
+        public DbSet<CRUD.Models.Conta>? Conta { get; set; }
+    }
+}
---
+++ CRUD/CRUD/Models/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models {
+    public class Conta {
+        public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "O nome deve ter no mínimo 4 caracateres")]
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [Display(Name = "Nome do cliente")]
+        public string Nome { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 1000, ErrorMessage = "O saldo deve estar entre 1 e 1000")]
+        public double Saldo { get; set; }
+    }
+}
